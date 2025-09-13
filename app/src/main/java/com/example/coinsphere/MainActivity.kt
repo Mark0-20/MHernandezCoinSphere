ecb7299ba89e52726a69be3165ec07894d095abb
Mark0-20 <marcoahm2005@gmail.com>
2025-09-12 21:08:44 -0600

    Se agregaron las InfoCards en el HomeScreen para visualizarlas, se agrego tambien el color del fondo

--- app/src/main/java/com/example/coinsphere/MainActivity.kt
+++ app/src/main/java/com/example/coinsphere/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +13,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.coinsphere.components.InfoCard
+import com.example.coinsphere.ui.theme.Background
 import com.example.coinsphere.ui.theme.CoinSphereTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoinSphereTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                  HomeScreen()
                 }
             }
         }
@@ -31,17 +32,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen(){
+    Column(modifier = Modifier
+        .background(color = Background)
+        .fillMaxSize()){
+        InfoCard("Global Market Cap", "$2.18T")
+        InfoCard("Fear & Greed", "Neutral (54)")
+        InfoCard("Altcoin Season", "No")
+    }
+
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true,
+    showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     CoinSphereTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
