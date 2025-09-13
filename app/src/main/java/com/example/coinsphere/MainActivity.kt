18b61ba610f2c9a3f6703e27264316509a2c54ca
Mark0-20 <marcoahm2005@gmail.com>
2025-09-13 00:02:55 -0600

    Se agrego el CryptoRow en el HomeScreen

--- app/src/main/java/com/example/coinsphere/MainActivity.kt
+++ app/src/main/java/com/example/coinsphere/MainActivity.kt
@@ -8,12 +8,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.coinsphere.components.CryptoRow
 import com.example.coinsphere.components.InfoCard
+import com.example.coinsphere.models.SampleData
+import com.example.coinsphere.models.monedas
 import com.example.coinsphere.ui.theme.Background
 import com.example.coinsphere.ui.theme.CoinSphereTheme
 
@@ -35,10 +41,17 @@ class MainActivity : ComponentActivity() {
 fun HomeScreen(){
     Column(modifier = Modifier
         .background(color = Background)
-        .fillMaxSize()){
+        .fillMaxSize()
+        .padding(16.dp)){
         InfoCard("Global Market Cap", "$2.18T")
         InfoCard("Fear & Greed", "Neutral (54)")
         InfoCard("Altcoin Season", "No")
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(SampleData.top10) { moneda ->
+                CryptoRow(crypto = moneda)
+            }
+        }
     }
 
 }
